bab6b9d7c0ceb1d9cb11fcc6b2a8c972efdeef27
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-11-02 01:42:54 +0600

    added random forest and updated data set

--- main.py
+++ main.py
@@ -19,20 +19,29 @@ train, test = split_(files)
 print("lbp feature_extractor")
 lbp = LocalBinaryPatterns(24, 8)
 lbp_trainer = Trainer(lbp, train, test)
+print("****************SVM****************")
 acc = lbp_trainer.train()
+print("****************RandomForest****************")
+rf_acc = lbp_trainer.train_rf()
 
 
 print("HoughTransform feature_extractor")
 ht = HoughTransform()
 ht_trainer = Trainer(ht, train, test)
+print("****************SVM****************")
 acc = ht_trainer.train()
+print("****************RandomForest****************")
+rf_acc = lbp_trainer.train_rf()
 
 
 
 print("HOG feature_extractor")
 hog = HOG()
 hog_trainer = Trainer(hog, train, test)
+print("****************SVM****************")
 acc = hog_trainer.train()
+print("****************RandomForest****************")
+rf_acc = lbp_trainer.train_rf()
 
 
 if __name__ == "__main__":
